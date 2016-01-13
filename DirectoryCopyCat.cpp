23ad2233475f27e54920b6036a643f8522095665
WhosMyName <WhosMyName@users.noreply.github.com>
2016-01-13 17:20:49 +0100

    removed all optinals cleaned code

--- DirectoryCopyCat.cpp
+++ DirectoryCopyCat.cpp
@@ -4,7 +4,7 @@ DirectoryCopyCat
 
 -1. Get Operating System; ------------------------------------------------------------y
 0.Ask for Volume/Season Renaming; ----------------------------------------------------y
-1.Get Directory; ---------------------------------------------------------------------WIP
+1.Get Directory; ---------------------------------------------------------------------WIP till the End
 2.Enable Volume 0 Support; -----------------------------------------------------------y
 3.Check Child Directorys; ------------------------------------------------------------y
 4.Count Child Directorys;-------------------------------------------------------------?
@@ -45,6 +45,7 @@ using namespace boost::filesystem;
 const int MAX_VOLUMES = 100;
 const int MAX_DIRECTORYS = 100;
 const int MAX_FILES = 500;
+/*-------------------> ??? <----------------------*/
 
 int Type = 0; //Determines type of Media to be focused on
 int Vol0 = 0; //Initialize only, shows inverted usage if Volume 0 functionallity
@@ -54,7 +55,7 @@ path Testing = (OriginalPath /= "Example");
 path ReName = OriginalPath.filename(); //Gets the Name of the Parent Folder, idk how but is does instead of parent_name(), but whatever ??? YOLO
 //string PathTraversal[MAX_VOLUMES][MAX_DIRECTORYS][MAX_FILES];
 
-void getRenameType(int Type, int Vol0){
+void getRenameType(){
 	string Media;
 	cout << "Would you like to Rename Series or Books?" << endl; //Output Self-Explainatory
 	cout << "Please answer with either \"Series\" or \"Books\" without the Quotes!" << endl; //Output Self-Explainatory
@@ -113,9 +114,9 @@ int main(){
         cout << "Other" << endl;
     }
 
-	//getRenameType(Type, Vol0);
-	//createdir();
+	getRenameType();
 	cyclepath(Testing);
+	//createdir();
 	return 0;
 }
 
@@ -138,58 +139,49 @@ void cyclepath(path p){
 				sort(v.begin(), v.end());             // sort, since directory iteration is not ordered on some file systems
 
 				stringstream ss3; //BackUp String for Path of Files with PrefixZero !Cast to String!
-				for (vec::const_iterator it (v.begin()); it != v.end(); ++it){ //Optional
-					cout << (*it).filename() << endl;
-				}
-				cout << endl << "<--------------------------------------------------------------------------------->" << endl << endl;
+				int count = 0; //Counts Directorys
+
 				for (vec::iterator it (v.begin()); it != v.end(); ++it){ //Iteration cycles through sorted Directory
 					if (!is_directory(*it)){
 						it = v.erase(it);
 						it--;
 					}
 					else{
-						cout << (*it).filename() << endl;
+						count++;
+						cout << "We found " << count << " Directorys so far!" << endl;
+						cout << (*it).filename() << " was the latest added Directory!" << endl << endl;
 					}
 				}
-				cout << endl << "<--------------------------------------------------------------------------------->" << endl << endl;
-				for (vec::const_iterator it (v.begin()); it != v.end(); ++it){ //Iteration cycles through sorted Directory
-					cout << (*it).filename() << endl;
-				}
-				cout << p << " is a Directory containing:" << endl; //Optional
-				string prev; //Optional
-				cout << "String!" << endl; //Optional
+				cout << "The total amount of Directorys is: " << count << endl;
 				cout << "Detecting Naming Scheme..." << endl;
 				if (Vol0 == 0){ //Check for Naming Scheme to use same algorythm to check for DiffDirs
 					for (vec::iterator it (v.begin()); it != v.end(); ++it){
-					for (int i = 1; i < 16; ++i){
-						stringstream ss2; //Creates a Stringstream for Counter Manipulation and conversion
-						ss2 << setfill('0') << setw(i) << Vol0; //Adds the Counter to the Stringstream
-						string str = ss2.str(); //Converts Stringstream to native String
-						string Prefixint(" Volume " + str); //Converted String Equals Volume + Prefix Int
-						string subdir = (*it).filename().string();
-						int f = subdir.find(Prefixint); //Optional
-						cout << "Result of find isdigit and: " << isdigit(subdir.find(Prefixint)) << endl; //Optional
-						cout <<"Found String at Position: " << f << endl; //Optional
-						if (subdir.find(Prefixint) == string::npos){
-							ss2.str(string()); //Clears the Stringstream
-							ss2 << setfill('0') << setw(i-1) << Vol0; //Adds the Counter to the Stringstream
+						for (int i = 1; i < 16; ++i){
+							stringstream ss2; //Creates a Stringstream for Counter Manipulation and conversion
+							ss2 << setfill('0') << setw(i) << Vol0; //Adds the Counter to the Stringstream
 							string str = ss2.str(); //Converts Stringstream to native String
 							string Prefixint(" Volume " + str); //Converted String Equals Volume + Prefix Int
-							cout << isdigit(subdir.find(Prefixint)) << endl; //Optional
-							cout << Prefixint << "!!!!!!!!!" << endl; //Optional
-							ss3 << Prefixint;
-							cout << "SS3 equals to " << ss3.str() << endl; //Optional 
-							break;
+							string subdir = (*it).filename().string();
+							if (subdir.find(Prefixint) == string::npos){
+								ss2.str(string()); //Clears the Stringstream
+								ss2 << setfill('0') << setw(i-1) << Vol0; //Adds the Counter to the Stringstream
+								string str = ss2.str(); //Converts Stringstream to native String
+								string Prefixint(" Volume " + str); //Converted String Equals Volume + Prefix Int
+								cout << isdigit(subdir.find(Prefixint)) << endl; //Optional
+								cout << Prefixint << "!!!!!!!!!" << endl; //Optional
+								ss3 << Prefixint;
+								cout << "SS3 equals to " << ss3.str() << endl; //Optional 
+								break;
+							}
+							else{
+								cout << (*it).filename().string() << endl;
+								cout << Prefixint << endl;
+								cout << "Breaking Madness!!" << endl; //Optional
+								continue;
+							}
 						}
-						else{
-							cout << (*it).filename().string() << endl;
-							cout << Prefixint << endl;
-							cout << "Breaking Madness!!" << endl; //Optional
-							continue;
-						}
+						break;
 					}
-					break;
-				}
 				}
 				else{
 					for (vec::iterator it (v.begin()); it != v.end(); ++it){
@@ -199,12 +191,9 @@ void cyclepath(path p){
 							string str = ss2.str(); //Converts Stringstream to native String
 							string Prefixint(" Volume " + str); //Converted String Equals Volume + Prefix Int
 							string subdir = (*it).filename().string();
-							int f = subdir.find(Prefixint);
-							cout << "Result of find isdigit and: " << isdigit(subdir.find(Prefixint)) << endl;
-							cout <<"Found String at Position: " << f << endl;
 							if (subdir.find(Prefixint) != string::npos){
-								cout << isdigit(subdir.find(Prefixint)) << endl;
-								cout << Prefixint << "!!!!!!!!!" << endl;
+								cout << isdigit(subdir.find(Prefixint)) << endl; //Optional 
+								cout << Prefixint << "!!!!!!!!!" << endl; //Optional 
 								ss3 << Prefixint;
 								cout << "SS3 equals to " << ss3.str() << endl; //Optional 
 								break;
@@ -212,7 +201,7 @@ void cyclepath(path p){
 							else{
 								cout << (*it).filename().string() << endl;
 								cout << Prefixint << endl;
-								cout << "Breaking Madness!!" << endl;
+								cout << "Breaking Madness!!" << endl; //Optional 
 								continue;
 							}
 						}
@@ -232,7 +221,7 @@ void cyclepath(path p){
 						cout << DiffDirs << endl;
 					}
 					else{
-						cout << "SAME HERE!" << endl; //Pending
+						cout << "SAME HERE!" << endl; //Pending - Optional
 					}
 
 					// do other stuff here.            
@@ -254,57 +243,12 @@ void cyclepath(path p){
 
 /*
 if (temp == NULL){
-						temp = *it;
-						cout << *it << endl;
-					}
-					else if(temp != NULL && temp.string().compare((*it).string()) != 0){
-						temp = *it;
-						cout << *it << endl;
-						DiffDirs++;
-					}
-
-void cyclepath(path p){
-	DiffDirs = 0;
-	try{
-		if (exists(p)) {   // does p actually exist?
-			if (is_regular_file(p)){       // is p a regular file?   
-				cout << p << " size is " << file_size(p) << endl; //if it is output the filesize
-			}
-			else if (is_directory(p)){      // is p a directory?
-				cout << p << " is a directory containing:" << endl;
-				string prev;
-				cout << "String!" << endl;
-				recursive_directory_iterator dir(p);
-				cout << "Iter1" << endl;
-				recursive_directory_iterator end;
-				cout << "Iter2" << endl;
-				while (dir != end){
-					if (prev.empty() == true){
-						string prev = (*dir).path().string();
-						cout << "p p 	" << (*dir).path().string() << endl;
-					}
-					if ((*dir).path().filename() != prev){
-						string prev = dir->path().string();
-						cout << "wololololo" << endl;
-						DiffDirs++;
-						cout << DiffDirs << endl;
-					}
-
-					// do other stuff here.            
-					cout << "Do Stuff!" << endl;
-					++dir;
-				}
-			}
-			else{
-				cout << p << " exists, but is neither a regular file nor a directory\n";
-			}
-		}
-		else{
-			cout << p << " does not exist\n";
-		}
-	}	
-	catch (const filesystem_error& ex){
-		cout << ex.what() << endl;
-	}
+	temp = *it;
+	cout << *it << endl;
+}
+else if(temp != NULL && temp.string().compare((*it).string()) != 0){
+	temp = *it;
+	cout << *it << endl;
+	DiffDirs++;
 }
 */
